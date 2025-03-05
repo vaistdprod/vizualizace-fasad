8a1bf35790adf1e8ab9960c47b396c5c950b210f
Matěj Vais <vais@tdprod.cz>
2025-03-05 15:19:40 +0100

    Prod-ready v6

--- src/components/CollectionArchive/index.tsx
+++ src/components/CollectionArchive/index.tsx
@@ -18,7 +18,7 @@ export const CollectionArchive: React.FC<Props> = (props) => {
             if (typeof result === 'object' && result !== null) {
               return (
                 <div className="col-span-4" key={index}>
-                  <Card className="h-full" doc={result} relationTo="aktuality" showCategories />
+                  <Card className="h-full" doc={result} _relationTo="aktuality" showCategories />
                 </div>
               )
             }
