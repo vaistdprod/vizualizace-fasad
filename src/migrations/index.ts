f07ff5d293d6eea4381aef9ca695e6e68e1bc413
Matěj Vais <vais@tdprod.cz>
2025-03-05 20:56:26 +0100

    Production ready final

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20250305_124204 from './20250305_124204';
+import * as migration_20250305_195034 from './20250305_195034';
 
 export const migrations = [
   {
-    up: migration_20250305_124204.up,
-    down: migration_20250305_124204.down,
-    name: '20250305_124204'
+    up: migration_20250305_195034.up,
+    down: migration_20250305_195034.down,
+    name: '20250305_195034'
   },
 ];
