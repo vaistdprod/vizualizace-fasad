1a78b981f49a1cd788ee019668afb9473dd3ae7e
Matěj Vais <vais@tdprod.cz>
2025-03-10 10:58:36 +0100

    3

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 import { withPayload } from '@payloadcms/next/withPayload'
-import redirects from './redirects.js'
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
